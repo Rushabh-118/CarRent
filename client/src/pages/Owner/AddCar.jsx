e0626db64870f092dd29dae4a5a2b72dd9a98171
Rushabh 118 <rushabhmistry118@gmail.com>
2025-07-09 12:45:47 +0530

    Addcar session done

--- client/src/pages/Owner/AddCar.jsx
+++ client/src/pages/Owner/AddCar.jsx
@@ -55,12 +55,60 @@ const AddCar = () => {
             </div>
             <div className='flex flex-col w-full'>
                 <label >Category</label>
-                <select>
-                  
+                <select onChange={e=> setCar({...car,category: e.target.value})} value={car.category} className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'>
+                  <option value="">select a Category</option>
+                  <option value="Sedan">Sedan</option>
+                  <option value="SUV">SUV</option>
+                  <option value="Van">Van</option>
                 </select>
-                <input type="text" placeholder="Give Model Name" className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none' value={car.category} onChange={e=> setCar({...car,category: e.target.value})} required/>
             </div>
         </div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
+          <div className='flex flex-col w-full'>
+                <label >Transmission</label>
+                <select onChange={e=> setCar({...car,transmission: e.target.value})} value={car.transmission} className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'>
+                  <option value="">select a Transmission</option>
+                  <option value="Autometic">Autometic</option>
+                  <option value="Manual">Manual</option>
+                  <option value="Semi-Autometic">Semi-Autometic</option>
+                </select>
+          </div>
+          <div className='flex flex-col w-full'>
+                <label >Fuel_Type</label>
+                <select onChange={e=> setCar({...car,fuelType: e.target.value})} value={car.fuelType} className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'>
+                  <option value="">select a Fuel_Type</option>
+                  <option value="Gas">Gas</option>
+                  <option value="Diesel">Diesel</option>
+                  <option value="Petrol">Petrol</option>
+                  <option value="Electric">Electric</option>
+                  <option value="Hybrid">Hybrid</option>
+                </select>
+            </div>
+            <div className='flex flex-col w-full'>
+                <label >Seating capacity</label>
+                <input type="number" placeholder="5" className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none' value={car.seatingCapacity} onChange={e=> setCar({...car,seatingCapacity: e.target.value})} required/>
+            </div>
+        </div>
+        <div className='flex flex-col w-full'>
+          <label >Location</label>
+          <select onChange={e=> setCar({...car,location: e.target.value})} value={car.location} className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'>
+                  <option value="">select a Location</option>
+                  <option value="Nadiad">Nadiad</option>
+                  <option value="Vadodara">Vadodara</option>
+                  <option value="Bharuch">Bharuch</option>
+                  <option value="Ahemdabad">Ahemdabad</option>
+                  <option value="Surat">Surat</option>
+                </select>
+        </div>
+        <div className='flex flex-col w-full'>
+                <label >Description</label>
+                <textarea rows={5} placeholder="Write About Car.." className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none' value={car.description} onChange={e=> setCar({...car,description: e.target.value})} required>
+                </textarea>
+        </div>
+        <button className='flex items-center gap-2 px-4 py-2.5 mt-4 bg-primary text-white rounded-md font-medium w-max cursor-pointer'> 
+          <img src={assets.tick_icon} alt="" />
+          Add Car
+        </button>
       </form>
     </div>
   )
