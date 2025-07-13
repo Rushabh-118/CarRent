1257474ac3bf6731f11f1911a1d74616552bf415
Rushabh 118 <rushabhmistry118@gmail.com>
2025-07-13 10:30:13 +0530

    image added into database

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,25 +1,49 @@
+import imagekit from "../config/imageKit.js";
+import Car from "../Models/Car.js";
 import User from "../Models/User.js";
+import fs from "fs";
 
-export const changeRoleToOwner = async(req,res) => {
-    try {
-        const {_id} = req.user;
-        await User.findByIdAndUpdate(_id, {role: "owner"})
-        res.json({success: true, message: "You are an owner now u can list cars"})
-    } catch (error) {
-        console.log(error.message);
-        res.json({success: false, message: error.message})
-    }
-}
+export const changeRoleToOwner = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    await User.findByIdAndUpdate(_id, { role: "owner" });
+    res.json({
+      success: true,
+      message: "You are an owner now u can list cars",
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
-export const addCar = async(req,res) => {
-    try {
-        const {_id} = req.user;
-        let car = JSON.parse(req.body.carData);
-        const imageFile = req.file;
-        
+export const addCar = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    let car = JSON.parse(req.body.carData);
+    const imageFile = req.file;
+    const fileBuffer = fs.readFileSync(imageFile.path);
+    const responce = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/cars",
+    });
 
-    } catch (error) {
-        console.log(error.message);
-        res.json({success: false, message: error.message})
-    }
-}
+    var imageURL = imagekit.url({
+      path: responce.filePath,
+      transformation: [
+        {width: '1280'},
+        {quality: 'auto'},
+        {format: 'webp'}
+      ]
+    });
+
+    const image = imageURL;
+    await Car.create({...car, owner: _id, image})
+    res.json({success: true, message: "Car Added"})
+
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
