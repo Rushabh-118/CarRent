f3f1b7e3818c36dd25e6b1b3c80c02b8e96d9f80
Rushabh 118 <rushabhmistry118@gmail.com>
2025-07-13 09:48:48 +0530

    multer added

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,10 +1,22 @@
-import User from "../Models/User";
+import User from "../Models/User.js";
 
 export const changeRoleToOwner = async(req,res) => {
     try {
         const {_id} = req.user;
         await User.findByIdAndUpdate(_id, {role: "owner"})
         res.json({success: true, message: "You are an owner now u can list cars"})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
+
+export const addCar = async(req,res) => {
+    try {
+        const {_id} = req.user;
+        let car = JSON.parse(req.body.carData);
+        const imageFile = req.file;
+
     } catch (error) {
         console.log(error.message);
         res.json({success: false, message: error.message})
